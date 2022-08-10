e5029088b8fae20b8aaf8b94ad6096f547b77232
Kokila Widyaratna <kokilaw@KokilaMBP.local>
2022-08-11 03:25:13 +0530

    Javadoc comments added for service level

--- src/main/java/io/kokilaw/banking/service/AccountService.java
+++ src/main/java/io/kokilaw/banking/service/AccountService.java
@@ -9,14 +9,57 @@ import java.math.BigDecimal;
  */
 public interface AccountService {
 
+    /**
+     * Creates an account and returns the created account object
+     *
+     * @param account account object
+     * @return the created account
+     * @throws io.kokilaw.banking.exception.CurrencyNotSupportedException If {@link AccountDTO#getCurrencyCode()}
+     *                                                                    is not supported
+     */
     AccountDTO createAccount(AccountDTO account);
 
+    /**
+     * Returns the account if available
+     *
+     * @param accountId id of the account
+     * @return the account object
+     * @throws io.kokilaw.banking.exception.AccountNotFoundException If account is not found for
+     *                                                               passed accountId
+     */
     AccountDTO getAccount(Long accountId);
 
+    /**
+     * Updates the existing account and return updated account
+     *
+     * @param accountId id of the account
+     * @param account   account object
+     * @return the updated account object
+     * @throws io.kokilaw.banking.exception.AccountNotFoundException      If account is not found for
+     *                                                                    passed accountId
+     * @throws io.kokilaw.banking.exception.CurrencyNotSupportedException If {@link AccountDTO#getCurrencyCode()}
+     *                                                                    is not supported
+     */
     AccountDTO updateAccount(Long accountId, AccountDTO account);
 
+    /**
+     * Updates the balance of the account
+     *
+     * @param accountId id of the account
+     * @param balance   balance value needs to be updated with
+     * @return the updated account object
+     * @throws io.kokilaw.banking.exception.AccountNotFoundException If account is not found for
+     *                                                               passed accountId
+     */
     AccountDTO updateAccountBalance(Long accountId, BigDecimal balance);
 
+    /**
+     * Deletes the account if available
+     *
+     * @param accountId id of the account needs to be deleted
+     * @throws io.kokilaw.banking.exception.AccountNotFoundException If account is not found for
+     *                                                               passed accountId
+     */
     void deleteAccount(Long accountId);
 
 }
--- src/main/java/io/kokilaw/banking/service/TransactionService.java
+++ src/main/java/io/kokilaw/banking/service/TransactionService.java
@@ -9,10 +9,40 @@ import java.util.List;
  */
 public interface TransactionService {
 
+    /**
+     * Returns all the transactions for an account
+     *
+     * @param accountId id of the account
+     * @return list of available transactions for account
+     * @throws io.kokilaw.banking.exception.AccountNotFoundException If account is not found for
+     *                                                               passed accountId
+     */
     List<TransactionDTO> getTransactions(Long accountId);
 
+    /**
+     * Creates transaction for an account
+     *
+     * @param accountId      id of the account
+     * @param transactionDTO transaction object
+     * @return the created transaction
+     * @throws io.kokilaw.banking.exception.AccountNotFoundException            If account is not found for
+     *                                                                          passed accountId
+     * @throws io.kokilaw.banking.exception.InsufficientAccountBalanceException If account does contain sufficient funds
+     *                                                                          to proceed with transaction
+     */
     TransactionDTO createTransaction(Long accountId, TransactionDTO transactionDTO);
 
+    /**
+     * Returns a specific transaction for an account
+     *
+     * @param accountId     id of the account
+     * @param transactionId id of the transaction
+     * @return the transaction
+     * @throws io.kokilaw.banking.exception.AccountNotFoundException     If account is not found for
+     *                                                                   passed accountId
+     * @throws io.kokilaw.banking.exception.TransactionNotFoundException If transaction is not found for
+     *                                                                   passed transactionId
+     */
     TransactionDTO getTransaction(Long accountId, Long transactionId);
 
 }

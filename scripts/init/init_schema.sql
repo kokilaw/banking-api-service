dfa823d6e64ccd92e73539e25c2343b025073627
kokilaw <kokilapc@gmail.com>
2022-08-11 21:21:15 +0530

    Schema updated

--- scripts/init/init_schema.sql
+++ scripts/init/init_schema.sql
@@ -16,13 +16,14 @@ ALTER TABLE bank_user
 CREATE TABLE account
 (
     id            BIGSERIAL,
-    user_id       BIGSERIAL,
+    user_id       BIGINT                      NOT NULL,
     balance       BIGINT                      NOT NULL,
     currency_code VARCHAR(3)                  NOT NULL,
     created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     updated_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES bank_user (id)
+    FOREIGN KEY (user_id) REFERENCES bank_user (id),
+    CONSTRAINT positive_account_balance CHECK (balance >= 0)
 );
 ALTER TABLE account
     OWNER TO postgres;
@@ -30,7 +31,7 @@ ALTER TABLE account
 CREATE TABLE transaction
 (
     id               BIGSERIAL,
-    account_id       BIGSERIAL                   NOT NULL,
+    account_id       BIGINT                      NOT NULL,
     transaction_type VARCHAR(10)                 NOT NULL,
     amount           BIGINT                      NOT NULL,
     created_at       TIMESTAMP WITHOUT TIME ZONE NOT NULL,
